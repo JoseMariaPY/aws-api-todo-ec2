e8d4d834189f158c05837a97053874def50383cf
jose.duarte.dev <josemduarte91@gmail.com>
2025-04-05 16:22:28 -0300

    settings

--- todo/settings.py
+++ todo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2kw0&g452b8#fk$yn2fuiwwev#3x07kua%mw7487cb&k=j8zbr
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['54.157.13.109', 'localhost']
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
